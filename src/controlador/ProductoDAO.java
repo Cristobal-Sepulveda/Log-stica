b7f561ee83599264663894cd81b0ca9d9fa04a77
Cristobal-Sepulveda <sepulveda.cristobal.ignacio@gmail.com>
2021-11-27 22:41:05 -0300

    Third Commit: ProductoDao has been updated

--- src/controlador/ProductoDAO.java
+++ src/controlador/ProductoDAO.java
@@ -13,17 +13,17 @@ public class ProductoDAO
 {
      public boolean ingresarProducto(Producto producto)
     {
-        boolean resultado=false;
+        boolean resultado= false;
         try {
-            Connection con= Conexion.getConexion();
+            Connection con = Conexion.getConexion();
             String query= "INSERT INTO PRODUCTO (codigo, nombre, tipoProducto, subTipoProducto, Precio) VALUES (?,?,?,?,?)";
-            PreparedStatement ps= con.prepareStatement(query);
+            PreparedStatement ps = con.prepareStatement(query);
             ps.setString(1, producto.getCodigo());
             ps.setString(2, producto.getNombre());
             ps.setString(3, producto.getTipoProducto());
             ps.setString(4, producto.getSubTipoProducto());
             ps.setInt(5, producto.getPrecio());
-            resultado= ps.executeUpdate() == 1;
+            resultado = ps.executeUpdate() == 1;
             ps.close();
         } catch (SQLException ex) {
             Logger.getLogger(ProductoDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -37,13 +37,13 @@ public class ProductoDAO
     {
         Producto producto=null;
         try {
-            Connection con= Conexion.getConexion();
-            String query= "SELECT * FROM PRODUCTO WHERE codigo = ?";
-            PreparedStatement ps=con.prepareStatement(query);
+            Connection con = Conexion.getConexion();
+            String query = "SELECT * FROM PRODUCTO WHERE codigo = ?";
+            PreparedStatement ps = con.prepareStatement(query);
             ps.setString(1, codigo);
-            ResultSet rs= ps.executeQuery();
+            ResultSet rs = ps.executeQuery();
             while(rs.next())
-                producto= new Producto(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5));
+                producto = new Producto(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5));
             ps.close();
         } catch (SQLException ex) {
             Logger.getLogger(ProductoDAO.class.getName()).log(Level.SEVERE, null, ex);
@@ -53,13 +53,13 @@ public class ProductoDAO
         return producto;
     }
     
-    public boolean eliminarAlumno(String codigo)
+    public boolean eliminarProducto(String codigo)
     {
-        boolean resultado=false;
+        boolean resultado = false;
         try {
-            Connection con= Conexion.getConexion();
-            String query= "DELETE FROM PRODUCTO WHERE RUT=?";
-            PreparedStatement ps=con.prepareStatement(query);
+            Connection con = Conexion.getConexion();
+            String query = "DELETE FROM PRODUCTO WHERE codigo = ?";
+            PreparedStatement ps = con.prepareStatement(query);
             ps.setString(1, codigo);
             resultado=ps.executeUpdate()==1;
             ps.close();
@@ -71,21 +71,20 @@ public class ProductoDAO
         }
         return resultado;
     }
-    public boolean modificarAlumno(Producto producto)
+    public boolean modificarProducto(Producto producto)
     {
         boolean resultado=false;
         try {
-            Connection con =Conexion.getConexion();
-            String query="update alumno set nombre=?,nota1=?,nota2=?,nota3=?,trabajogrupal=? where rut=?";
-            PreparedStatement ps=con.prepareStatement(query);
+            Connection con = Conexion.getConexion();
+            String query = "UPDATE PRODUCTO SET codigo = ?, nombre = ?,tipoProducto = ?, subTipoProducto = ?, precio = ?";
+            PreparedStatement ps = con.prepareStatement(query);
             ps.setString(1, producto.getCodigo());
             ps.setString(2, producto.getNombre());
             ps.setString(3, producto.getTipoProducto());
             ps.setString(4, producto.getSubTipoProducto());
             ps.setInt(5, producto.getPrecio());
-            resultado=ps.executeUpdate()==1;
+            resultado = ps.executeUpdate() == 1;
             ps.close();
-            
         } catch (SQLException ex) {
             Logger.getLogger(ProductoDAO.class.getName()).log(Level.SEVERE, null, ex);
         } catch (ClassNotFoundException ex) {
@@ -96,16 +95,16 @@ public class ProductoDAO
     
     public ArrayList<Producto> obtenerTodo()
     {
-        ArrayList <Producto> productos=new ArrayList<Producto>();
+        ArrayList <Producto> productos = new ArrayList<Producto>();
         try {
-            Connection con=Conexion.getConexion();
+            Connection con = Conexion.getConexion();
             String query="SELECT * FROM PRODUCTO";
-            PreparedStatement ps=con.prepareStatement(query);
-            ResultSet rs=ps.executeQuery();
+            PreparedStatement ps = con.prepareStatement(query);
+            ResultSet rs = ps.executeQuery();
             Producto producto;
             while(rs.next())
             {
-                producto=new Producto(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5));
+                producto = new Producto(rs.getString(1), rs.getString(2), rs.getString(3), rs.getString(4), rs.getInt(5));
                 productos.add(producto);
             }
             ps.close();
@@ -116,5 +115,4 @@ public class ProductoDAO
         }
         return productos;
     }
-       
 }
